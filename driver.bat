bdb3495d4234a161c7695e492b3f9468e4d75dff
Simon Crase <simon@greenweaves.nz>
2019-06-04 15:18:23 +1200

    Added driver.bat, which had been left in astro by mistake

---
+++ driver.bat
@@ -0,0 +1,14 @@
+@echo %time%
+python leighton.py -f 0 -t 2880 -s 1 -l -80 -c
+python leighton.py -f 0 -t 2880 -s 1 -l -70 -c
+python leighton.py -f 0 -t 2880 -s 1 -l -50 -c
+python leighton.py -f 0 -t 2880 -s 1 -l -30 -c
+python leighton.py -f 0 -t 2880 -s 1 -l -10 -c
+python leighton.py -f 0 -t 2880 -s 1 -l 0 -c
+python leighton.py -f 0 -t 2880 -s 1 -l 10 -c
+python leighton.py -f 0 -t 2880 -s 1 -l 30  -c
+python leighton.py -f 0 -t 2880 -s 1 -l 50 -c
+python leighton.py -f 0 -t 2880 -s 1 -l 70 -c
+python leighton.py -f 0 -t 2880 -s 1 -l 90 -c
+
+@echo %time%
