ddc11499b82afba2ed82b3fd3f34777e7e48b9d5
Simon Crase <simon@greenweaves.nz>
2022-10-17 15:32:00 +1300

    Closed #14

--- cores.py
+++ cores.py
@@ -1,4 +1,6 @@
-# Copyright (C) 2017-2019 Greenweaves Software Limited
+#!/usr/bin/env python
+
+# Copyright (C) 2017-2022 Greenweaves Software Limited
 
 # This is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -22,65 +24,69 @@
 
 # For an explanation, see https://docs.python.org/3.5/library/multiprocessing.html, which warns
 #    Functionality within this package requires that the __main__ module be importable by the children.
-#    This is covered in Programming guidelines however it is worth pointing out here. 
+#    This is covered in Programming guidelines however it is worth pointing out here.
 #    This means that some examples, such as the multiprocessing.pool.Pool examples will not work
 #    in the interactive interpreter.
 
-import multiprocessing as mp, leighton, time, getopt,sys
+from getopt          import getopt, GetoptError
+from leighton        import main
+from multiprocessing import current_process, Manager, Pool
+from os              import environ
+from sys             import argv, exit
+from time            import time
 
-# This will be called by each process when it starts
 
 def init_worker(mps, fps, cut):
+    ''' This will be called by each process when it starts'''
     global memorizedPaths, filepaths, cutoff
     global DG
 
-    print ('process initializing', mp.current_process())
+    print ('process initializing', current_process())
     memorizedPaths, filepaths, cutoff = mps, fps, cut
     DG                                = 1
 
-# Used to execute the model once for each 
+
 def work(item):
+    ''' Used to execute the model once for each'''
     print ('Processing {0}'.format(item))
-    leighton.main(item.split())
+    main(item.split())
 
 def help():
     print ('python cores.py datafile')
-    
+
 def parse_command_line(argv):
     input_file  = 'cores.dat'
-    if len(sys.argv[1:])>0:
+    if len(argv[1:])>0:
         try:
-            opts, args = getopt.getopt(sys.argv[1:],'',[])
-            for opt, arg in opts: 
+            opts, args = getopt(argv[1:],'',[])
+            for opt, arg in opts:
                 pass
             input_file = args[0]
-        except getopt.GetoptError:
+        except GetoptError:
             help()
-            sys.exit(2)
+            exit(2)
     return input_file
 
 if __name__ == '__main__':
-    # Detect whether running undrt IDE or command line - 
-    # see https://stackoverflow.com/questions/552627/python-ide-vs-cmd-line-detection
-    if sys.stdin.isatty():
-        start_time     = time.time() 
-        manager        = mp.Manager()
+
+    if 'WINGDB_ACTIVE' not in environ.keys(): # Detect whether running under IDE or command line
+        start_time     = time()
+        manager        = Manager()
         memorizedPaths = manager.dict()
         filepaths      = manager.dict()
         cutoff         = 1
-    
-        # Create process pool to use all available CPUs
-        pool           = mp.Pool(initializer=init_worker, initargs=(memorizedPaths, filepaths, cutoff))
-        input_file     = parse_command_line(sys.argv[1:])
+        pool           = Pool(initializer = init_worker,   # Create process pool to use all available CPUs
+                              initargs    = (memorizedPaths, filepaths, cutoff))
+        input_file     = parse_command_line(argv[1:])
         print ('Opening {0}'.format(input_file))
         with open(input_file) as file:
             for _ in pool.imap_unordered(work,
                                          [line.strip() for line in  file.readlines() if len(line.strip())>0],
                                          chunksize=1):
                 pass
-            
+
             pool.close()
             pool.join()
-            print('--- %s seconds ---' % (time.time() - start_time))            
+            print('--- %s seconds ---' % (time() - start_time))
     else:
-        print ("cores.py must be executed from command line, not from IDE")
+        print ('cores.py must be executed from command line, not from IDE')
